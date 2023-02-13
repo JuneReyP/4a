49ecbdefef06c4a2925474d22cf9d3acea659518
JuneReyP <junereypalabrica@gmail.com>
2023-02-13 14:07:20 +0800

    first upload

---
+++ sample.js
@@ -0,0 +1,72 @@
+document.getElementById('display').innerHTML = "Sample JS script";
+document.getElementById('display').style.backgroundColor = 'orange';
+document.getElementById('display').style.color = 'white';
+
+//sdshdfskfjsfjkbjsv
+
+/*
+Using variables:
+
+var
+let
+const
+
+
+
+*/
+
+var num = 10;
+let num3 = 11;
+const sample = 12;
+var name = "Juan";
+var name2 = "Dela Cruz";
+var sampleBol = true;
+
+console.log(name + " "+ name2 + " J'r" + " 24");
+
+// string
+//numbers
+//boolean
+
+//JS operators
+/*
+=
++
+*
+/
+%
+++
+--
+*/
+
+/* Comparison Operator
+== 	equal to
+=== 	equal value and equal type
+!= 	not equal
+!== 	not equal value or not equal type
+> 	greater than
+< 	less than
+>= 	greater than or equal to
+<= 	less than or equal to
+? 	ternary operator
+*/
+
+console.log(3 == 2); //false
+console.log(3 !== "3"); //true
+console.log(3 == 3); //true
+console.log(3 == "3"); //true
+console.log(3 === "3"); //false
+console.log(3 > 2); //true
+console.log(2 > 2); //true
+console.log(1 >= 2); //false
+
+// condition ? exprIfTrue : exprIfFalse
+
+(3 == 3) ? console.log("Same number") : console.log("not equal");
+
+
+
+
+
+
+
